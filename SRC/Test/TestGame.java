5bc7cca9c59b92911e0da0f446313df0da4f5be8
Alvin Kwan <alvinkwan24@gmail.com>
2019-05-20 16:04:52 -0700

    Update TestGame.java

--- SRC/Test/TestGame.java
+++ SRC/Test/TestGame.java
@@ -1,31 +1,28 @@
-//package Test;
-//
-//import Model.Exception.OutOfTimeException;
-//import Model.PortalGame;
-//import UI.PortalMazes;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//public class TestGame {
-//    private PortalGame g;
-//    private PortalMazes p;
-//
-//    @BeforeEach
-//    public void runBefore(){
-//        g = new PortalGame();
-//    }
-//
-//    @Test
-//    public void testThrownOutOfTimeException() throws OutOfTimeException{
-//        try {
-//            g = new PortalGame();
-//            addTimer();
-//        } catch (OutOfTimeException e) {
-//            System.out.printf("caught ittt");
-//        }
-//    }
-//
-//
-//
-//
-//}
+package Test;
+
+import Model.Exception.OutOfTimeException;
+import Model.PortalGame;
+import UI.PortalMazes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestGame {
+   private PortalGame g;
+   private PortalMazes p;
+
+   @BeforeEach
+   public void runBefore(){
+       g = new PortalGame();
+   }
+
+   @Test
+   public void testThrownOutOfTimeException() throws OutOfTimeException{
+       try {
+           g = new PortalGame();
+           addTimer();
+         
+       } catch (OutOfTimeException e) {
+           System.out.printf("caught ittt");
+       }
+   }
+}
